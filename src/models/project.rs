9ab18dcdfff5a8c7cc70131d5c6d86a887ef58dc
fernando.bandeira94 <fernando.bandeira94@gmail.com>
2026-03-16 13:59:56 -0300

    chore(fn5j): Replace inline model copies with re-exports from djinn-core

--- src/models/project.rs
+++ src/models/project.rs
@@ -1,14 +1,1 @@
-use serde::{Deserialize, Serialize};
-
-/// A registered project.
-#[derive(Clone, Debug, Serialize, Deserialize, sqlx::FromRow)]
-pub struct Project {
-    pub id: String,
-    pub name: String,
-    pub path: String,
-    pub created_at: String,
-    pub target_branch: String,
-    pub auto_merge: bool,
-    pub sync_enabled: bool,
-    pub sync_remote: Option<String>,
-}
+pub use djinn_core::models::project::*;
